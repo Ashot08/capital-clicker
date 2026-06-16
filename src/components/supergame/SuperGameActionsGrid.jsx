9b14f002b06b0b90b2568860edeb61d843167671
Ashot08 <albertguzeev@yandex.ru>
2026-06-16 18:32:40 +0700

    fix bear

--- src/components/supergame/SuperGameActionsGrid.jsx
+++ src/components/supergame/SuperGameActionsGrid.jsx
@@ -5,13 +5,15 @@ import ActionCard from "@/components/home/ActionCard";
 import { rulesService } from "@/services/RulesService";
 import { actionsGridRewardsService } from "@/services/ActionsGridRewardsService";
 import { useFullscreen } from "@/hooks/useFullScreen";
-import { isMobile } from "@/lib/platform";
+import { isMobile } from 'react-device-detect';
 import { useRef, useEffect } from "react";
 import { playSound } from "@/audio/manager";
 import { useNavigate } from "react-router-dom";
 import useChestStore from "@/stores/useChestStore";
+import {clsx} from "clsx";
+import EnergyDisplay from "@/components/home/EnergyDisplay.jsx";
 
-export default function SuperGameActionsGrid() {
+export default function SuperGameActionsGrid({energy = ''}) {
   const navigate = useNavigate();
   const { isFullscreen, openFullscreen } = useFullscreen();
   const timeoutRef = useRef(null);
@@ -54,78 +56,101 @@ export default function SuperGameActionsGrid() {
   };
 
   return (
-    <div className="w-full flex justify-center min-w-[18rem]">
-      {/* мобильная и десктопная верстка без изменений */}
-      <div className="w-full sm:hidden">
-        <div className="w-full grid grid-cols-3 gap-3 max-w-88.5 mx-auto">
-          <TreasureChest
-            onClick={handleChestClick}
-            progress={chestPercent}
-            className="max-w-[6.875rem] aspect-[110/69]"
-          />
-          <div className="max-w-[6.875rem] aspect-[110/69]"></div>
-          <div className="max-w-[6.875rem] aspect-[110/69]"></div>
+      <>
+        {
+            !isMobile && <div className={clsx(!isMobile && ["flex", "justify-center", "mb-8", "sm:mb-4", "lg:mb-3"])}>
+              <EnergyDisplay
+                  energy={energy}
+                  iconClasses="w-8 h-8 sm:w-8 sm:h-8"
+                  textClasses="text-[1.5rem] sm:text-[1.5rem]"
+              />
+            </div>
+        }
+        <div className="w-full flex justify-center min-w-[18rem]">
+          {/* мобильная и десктопная верстка без изменений */}
+          <div className="w-full sm:hidden">
+            <div className="w-full grid grid-cols-3 gap-3 max-w-88.5 mx-auto">
+              <TreasureChest
+                  onClick={handleChestClick}
+                  progress={chestPercent}
+                  className="max-w-[6.875rem] aspect-[110/69]"
+              />
+              <div className="max-w-[6.875rem] aspect-[110/69] mb-1 flex items-center">
+                {
+                    isMobile && <EnergyDisplay
+                        energy={energy}
+                        iconClasses="w-8 h-8 sm:w-8 sm:h-8"
+                        textClasses="text-[1.5rem] sm:text-[1.5rem]"
+                    />
+                }
+              </div>
 
-          <ActionCard
-            onClick={handleFullscreenClick}
-            to="/shop"
-            icon="shop"
-            label="Магазин"
-            className="max-w-[6.875rem] aspect-[110/88]"
-          />
-          <ActionCard
-            onClick={() => {
-              handleFullscreenClick();
-              rulesService.openRulesModal("/data/superRules.json", true);
-            }}
-            icon="info"
-            label="Правила"
-            className="border-white text-white max-w-[6.875rem] aspect-[110/88]"
-          />
-          <ActionCard
-            onClick={() => {
-              handleFullscreenClick();
-              actionsGridRewardsService.rewardsClick();
-            }}
-            icon="trophy"
-            label="Мои призы"
-            className="max-w-[6.875rem] aspect-[110/88]"
-          />
+
+              <div className="max-w-[6.875rem] aspect-[110/69]"></div>
+
+              <ActionCard
+                  onClick={handleFullscreenClick}
+                  to="/shop"
+                  icon="shop"
+                  label="Магазин"
+                  className="max-w-[6.875rem] aspect-[110/88]"
+              />
+              <ActionCard
+                  onClick={() => {
+                    handleFullscreenClick();
+                    rulesService.openRulesModal("/data/superRules.json", true);
+                  }}
+                  icon="info"
+                  label="Правила"
+                  className="border-white text-white max-w-[6.875rem] aspect-[110/88]"
+              />
+              <ActionCard
+                  onClick={() => {
+                    handleFullscreenClick();
+                    actionsGridRewardsService.rewardsClick();
+                  }}
+                  icon="trophy"
+                  label="Мои призы"
+                  className="max-w-[6.875rem] aspect-[110/88]"
+              />
+            </div>
+          </div>
+
+          <div className="hidden sm:flex sm:gap-4 w-full mx-auto max-w-159.5">
+            <TreasureChest
+                onClick={handleChestClick}
+                progress={chestPercent}
+                className="max-w-[9rem] aspect-[144/115]"
+            />
+            <ActionCard
+                onClick={handleFullscreenClick}
+                to="/shop"
+                icon="shop"
+                label="Магазин"
+                className="max-w-[9rem] aspect-[144/115]"
+            />
+            <ActionCard
+                onClick={() => {
+                  handleFullscreenClick();
+                  rulesService.openRulesModal("/data/superRules.json", true);
+                }}
+                icon="info"
+                label="Правила"
+                className="max-w-[9rem] aspect-[144/115] border-white text-white"
+            />
+            <ActionCard
+                onClick={() => {
+                  handleFullscreenClick();
+                  actionsGridRewardsService.rewardsClick();
+                }}
+                icon="trophy"
+                label="Мои призы"
+                className="max-w-[9rem] aspect-[144/115]"
+            />
+          </div>
         </div>
-      </div>
+      </>
 
-      <div className="hidden sm:flex sm:gap-4 w-full mx-auto max-w-159.5">
-        <TreasureChest
-          onClick={handleChestClick}
-          progress={chestPercent}
-          className="max-w-[9rem] aspect-[144/115]"
-        />
-        <ActionCard
-          onClick={handleFullscreenClick}
-          to="/shop"
-          icon="shop"
-          label="Магазин"
-          className="max-w-[9rem] aspect-[144/115]"
-        />
-        <ActionCard
-          onClick={() => {
-            handleFullscreenClick();
-            rulesService.openRulesModal("/data/superRules.json", true);
-          }}
-          icon="info"
-          label="Правила"
-          className="max-w-[9rem] aspect-[144/115] border-white text-white"
-        />
-        <ActionCard
-          onClick={() => {
-            handleFullscreenClick();
-            actionsGridRewardsService.rewardsClick();
-          }}
-          icon="trophy"
-          label="Мои призы"
-          className="max-w-[9rem] aspect-[144/115]"
-        />
-      </div>
-    </div>
+
   );
 }
